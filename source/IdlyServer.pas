bbc8fe48d56fe24dae6def7f02bc9e146fe06ea5
Ozz Nixon <ozznixon@gmail.com>
2026-08-01 16:10:27 -0500

    Add IdlyServer program with basic setup
    Implements QOTD, a simple demonstration of how to register the RFC Engine.

---
+++ source/IdlyServer.pas
@@ -0,0 +1,35 @@
+(* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *)
+(*  Filename: Idly.Types.pas                                         *)
+(* Copyright: (C) 2026 by Brain Patchwork DX, LLC. build 1260801     *)
+(* ================================================================= *)
+(* Even though I author DXSock, a commercial socket suite ~ this was *)
+(* written to give people access to my talent for free.              *)
+(* ozznixon@gmail.com                                                *)
+(* Ozz Nixon                                                         *)
+(* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *)
+program IdlyServer;
+
+{$mode objfpc}{$H+}{$J-}
+
+uses
+  SysUtils,
+  Idly.Core,
+  Idly.Types,
+  Idly.RFC865;
+
+var
+  Engine: TIdlyEngine;
+  QOTDHandler: IIdlyProtocolHandler;
+begin
+  Engine := TIdlyEngine.Create;
+  QOTDHandler := TIdlyQOTDHandler.Create;
+  try
+    { Bind RFC 865 to port 17 }
+    Engine.RegisterRFC(17, QOTDHandler);
+    
+    WriteLn('Idly Socket Suite Initialized. Listening for RFC implementations...');
+    // Engine.Start; // Uncomment once event loop system hooks are defined
+  finally
+    Engine.Free;
+  end;
+end.
